fb5ae816ee305c8a3b9cc96a17ed1a52ce4433ad
vironika00 <159715559+vironika00@users.noreply.github.com>
2024-02-11 21:16:08 +0100

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -6,16 +6,16 @@ try {
 let [packname, ...author] = text.split('|')
 author = (author || []).join('|')
 let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.wm noureddine ouafy*'
+if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.wm anas gac*'
 let img = await m.quoted.download()
-if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.wm noureddine ouafy*'
+if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.wm anas cga*'
 stiker = await addExif(img, packname || global.packname, author || global.author)
 } catch (e) {
 console.error(e)
 if (Buffer.isBuffer(e)) stiker = e
 } finally {
 if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/noureddine_ouafy*'
+else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/_y._cx*'
 }}
 handler.help = ['wm']
 handler.tags = ['sticker']
