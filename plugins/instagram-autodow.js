2e30e5217dbe41c97cfbdf2438c5327fa57caa87
vironika00 <159715559+vironika00@users.noreply.github.com>
2024-02-24 19:52:01 +0100

    Update instagram-autodow.js

--- plugins/instagram-autodow.js
+++ plugins/instagram-autodow.js
@@ -4,12 +4,12 @@ const handler = async function (m, { conn }) {
     const args = m.text.split(' ');
     try {
         if (args.length > 0) {
-            m.reply('*المرجو الانتظار سيتم تحميل الفيديو بعد قليل*');
+            m.reply('*Downloading*');
             const url = args[0];
             let res = await (await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=5c168427edb54007b897cefe&url=${url}`)).json(); 
 
-            if (!res.result || res.result.length === 0) throw "لا يمكن العثور على الفيديو في الرابط";
-            conn.sendFile(m.chat, res.result[0], '', 'instagram.com/noureddine_ouafy', m);
+            if (!res.result || res.result.length === 0) throw "Error 404";
+            conn.sendFile(m.chat, res.result[0], '', '', m);
 
             for (let imgs of res.result) {   
                 let ban = m.mentionedJid[0] || m.sender || conn.parseMention(args[0]) || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || '';
